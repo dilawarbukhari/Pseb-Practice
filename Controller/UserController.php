ecd3b770ba9797b99f78562624289c399e8b0f89
dilawarbukhari <syedbadshahalinaqvi@gmail.com>
2026-04-08 16:49:28 +0500

    latest changes

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -3,32 +3,6 @@ require './Model/Model.php';
 
 class Controller extends Model
 {
-  public function handleProcess($process,$id,$accessToken)
-  {
-    switch ($process) {
-      case 'getAllUser':
-      
-        $data = $this->getAll($accessToken);
-        return json_encode($data);
-         case 'deleteUser':
-          $data = $this->deleteUser($id);
-          return json_encode($data);
-      default:
-        return json_encode(['message' => 'Invalid controller process']);
-    }
-  }
-  public function addLogic($process,$id,$name,$email,$phone,$address,$created){
-   switch ($process){    
-  case 'addUser':
-          $data = $this->addUser($name,$email,$phone,$address,$created);
-          return json_encode($data);
-          break;
-            case 'updateUser':
-          $data = $this->updateUser($id,$name,$email,$phone,$address,$created);
-          return json_encode($data);
-          break;
-  }
-  }
     public function simplifyLogic($input){
       $process= $input['process'];
       switch ($process){
@@ -36,8 +10,108 @@ class Controller extends Model
           $data = $this->registerUser($input);
           return json_encode($data);
           break;
+           case 'deletePermission':
+          $data = $this->deletePermission($input);
+          return json_encode($data);
+          break;
+             case 'updatePermission':
+          $data = $this->updatePermission($input);
+          return json_encode($data);
+          break;
+             case 'addPermission':
+          $data = $this->addPermission($input);
+          return json_encode($data);
+          break;
+           case 'addUser':
+          $data = $this->addUser($input);
+          return json_encode($data);
+          break;
+          case 'addRole':
+           $data = $this->addRole($input);
+          return json_encode($data);
+          break;
+            case 'updateRolePermission':
+          $data = $this->updateRolePermission($input);
+          return json_encode($data);
+          break;
+            case 'changePassword':
+          $data = $this->changePassword($input);
+          return json_encode($data);
+          break;
+           case 'updateUserDetail':
+          $data = $this->updateUserDetail($input);
+          return json_encode($data);
+          break;
+           case 'getUserDetail':
+          $data = $this->getUserDetail($input);
+          return json_encode($data);
+          break;
+          case 'deleteRole':
+          $data = $this->deleteRole($input);
+          return json_encode($data);
+          break;
+          case 'getRolePermission':
+          $data = $this->getRolePermission($input);
+          return json_encode($data);
+          break;
+          case 'updateRole':
+          $data = $this->updateRole($input);
+          return json_encode($data);
+          break;
+           case 'updateUser':
+          $data = $this->updateUser($input);
+          return json_encode($data);
+          break;
+           case 'getAllCategory':  
+        $data = $this->getAllCategory($input);
+        return json_encode($data);
+        break;
+          case 'getAllUser':  
+        $data = $this->getAllUser($input);
+        return json_encode($data);
+        break;
+         case 'getAllPermission':  
+        $data = $this->getAllPermission($input);
+        return json_encode($data);
+        break;
+          case 'getRoles':  
+        $data = $this->getAllroles($input);
+        return json_encode($data);
+        break;
+        case 'getAllProduct':  
+        $data = $this->getAllProduct($input);
+        return json_encode($data);
+        break;
+           case 'deleteCategory':
+          $data = $this->deleteCategory($input);
+          return json_encode($data);
+          break;
+           case 'deleteUser':
+          $data = $this->deleteUser($input);
+          return json_encode($data);
+          break;
+           case 'deleteProduct':
+          $data = $this->deleteProduct($input);
+          return json_encode($data);
+          break;
           case 'loginUser':
              $data = $this->login($input);
+          return json_encode($data);
+          break;
+          case 'addCategory':
+          $data = $this->addCategory($input);
+          return json_encode($data);
+          break;
+          case 'addProduct':
+          $data = $this->addProduct($input);
+          return json_encode($data);
+          break;
+         case 'updateCategory':
+          $data = $this->updateCategory($input);
+          return json_encode($data);
+          break;
+             case 'updateProduct':
+          $data = $this->updateProduct($input);
           return json_encode($data);
           break;
    } 
--- Route.php
+++ Route.php
@@ -1,27 +1,84 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Origin: http://localhost:4200");
+
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Content-Type: application/json");
 require('./Controller/UserController.php');
 $input = json_decode(file_get_contents("php://input"), true);
-$process = $input['process'] ?? '';
-$accessToken = $input['accessToken'] ?? '';
-$id = $input['userId'] ?? null;
-$name = $input['Name'] ?? null;
-$email = $input['Email'] ?? null;
-$phone = $input['Phone'] ?? null;
-$address = $input['Address'] ?? null;
-$created = $input['Created'] ?? null;
+$process= $input['process'];
 
 $controller = new Controller();
 switch ($process) {
-  case 'getAllUser':
-    $response = $controller->handleProcess($process, $id, $accessToken);
+  case 'getAllCategory':
+    $response = $controller->simplifyLogic($input);
     echo $response;
     break;
-
+    case 'getRolePermission':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+     case 'updateRolePermission':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+     case 'getAllProduct':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+    case 'getRoles':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+    case 'updateRole':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+    case 'deleteRole':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+     case 'getAllPermission':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+     case 'getAllUser':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+     case 'updatePermission':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+    case 'deletePermission':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+    case 'addPermission':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+    case 'addUser':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+    case 'changePassword':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+     case 'getUserDetail':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+       case 'updateUserDetail':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+     case 'updateUser':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
   case 'registerUser':
     $response = $controller->simplifyLogic($input);
     echo $response;
@@ -30,18 +87,36 @@ switch ($process) {
     $response = $controller->simplifyLogic($input);
     echo $response;
     break;
-
-  case 'deleteUser':
-    $response = $controller->handleProcess($process, $id,$accessToken);
+  case 'deleteCategory':
+    $response = $controller->simplifyLogic($input);
     echo $response;
     break;
-
-  case 'addUser':
-    $response = $controller->addLogic($process, $id, $name, $email, $phone, $address, $created);
+    case 'deleteUser':
+    $response = $controller->simplifyLogic($input);
     echo $response;
     break;
-  case 'updateUser':
-    $response = $controller->addLogic($process, $id, $name, $email, $phone, $address, $created);
+    case 'addRole':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+    case 'deleteProduct':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+  case 'addCategory':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+  case 'addProduct':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+  case 'updateCategory':
+    $response = $controller->simplifyLogic($input);
+    echo $response;
+    break;
+      case 'updateProduct':
+    $response = $controller->simplifyLogic($input);
     echo $response;
     break;
 }
